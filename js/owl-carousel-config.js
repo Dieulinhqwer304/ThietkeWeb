777ec379b95e57ed450265df2481fe68ff80e5af
Hoa1301 <hoacutehd2003@gmail.com>
2025-05-18 16:36:28 +0700

    new update

---
+++ js/owl-carousel-config.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#dssanphammoi").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 }
+    }
+  });
+});
